5368d3aae924d57ed12d7ec6a5c46aaa730af769
zcogz <zac.cogdill@gmail.com>
2025-08-04 13:09:39 -0400

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from agents.scan_agent import run_scan
-
-if __name__ == '__main__':
-    name = input('Enter your name: ')
-    email = input('Enter your email: ')
-    results = run_scan(name, email)
-    print('\nScan Results:')
-    for result in results:
-        print(result)
